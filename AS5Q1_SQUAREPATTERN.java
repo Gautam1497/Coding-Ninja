6124797537ca378741296e9708d727c781e369be
gautamsingh1497 <gk1497singh@gmail.com>
2024-02-17 12:30:04 +0530

    Assignment 5 ques 1 Square Pattern

---
+++ AS5Q1_SQUAREPATTERN.java
@@ -0,0 +1,43 @@
+/*
+Code : Square Pattern
+
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+4444
+4444
+4444
+4444
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 50
+Sample Input 1:
+7
+Sample Output 1:
+7777777
+7777777
+7777777
+7777777
+7777777
+7777777
+7777777
+
+ */
+import  java.util.*;
+public class AS5Q1_SQUAREPATTERN {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n=sc.nextInt();
+        for (int i=0;i<n;i++)
+        {
+            for (int j=0;j<n;j++)
+            {
+                System.out.print(n+" ");
+            }
+            System.out.println();
+        }
+    }
+}
