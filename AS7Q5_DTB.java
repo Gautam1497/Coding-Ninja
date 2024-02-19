f67222e3e8d18a64e17289fe803f6d48f5d1e87f
gautamsingh1497 <gk1497singh@gmail.com>
2024-02-19 22:48:12 +0530

    Assignment 7 ques 5 Decimal to Binary

---
+++ AS7Q5_DTB.java
@@ -0,0 +1,40 @@
+/*
+Decimal to Binary
+
+Given a decimal number (integer N), convert it into binary and print.
+The binary number should be in the form of an integer.
+Note : The given input number could be large, so the corresponding binary number can exceed the integer range. So you may want to take the answer as long for CPP and Java.
+
+Input format :
+Integer N
+Output format :
+Corresponding Binary number (long)
+Constraints :
+0 <= N <= 10^5
+Sample Input 1 :
+12
+Sample Output 1 :
+1100
+Sample Input 2 :
+7
+Sample Output 2 :
+111
+ */
+import java.util.*;
+public class AS7Q5_DTB {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n=sc.nextInt();
+        String s;
+        String a = "";
+        while (n>0)
+        {
+            int temp=n%2;
+            s=Integer.toString(temp);
+            a=s+a;
+            n/=2;
+        }
+        System.out.print(a);
+    }
+}
