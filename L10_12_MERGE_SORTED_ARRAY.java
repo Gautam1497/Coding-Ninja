1e1b0a16522c10061ef748bc3ecdd91cdd8777d9
gautamsingh1497 <gk1497singh@gmail.com>
2024-02-26 11:57:37 +0530

    Lecture 10 ques 12 Code Merge Two Sorted Arrays

--- L10_12_MERGE_SORTED_ARRAY.java
+++ L10_12_MERGE_SORTED_ARRAY.java
@@ -45,7 +45,7 @@ Sample Output 2 :
 
 import java.util.Scanner;
 
-public class L10_12_MERGE_SORT {
+public class L10_12_MERGE_SORTED_ARRAY {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter number of test cases: ");
