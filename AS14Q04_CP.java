407675eda173b834173f1f94341f21deb74d8ed8
gautamsingh1497 <gk1497singh@gmail.com>
2024-04-29 22:09:52 +0530

    Assignment 14 ques 4 Check Palindrome (recursive)

---
+++ AS14Q04_CP.java
@@ -0,0 +1,37 @@
+/*
+
+
+Check whether a given String S is a palindrome using recursion. Return true or false.
+Input Format :
+String S
+Output Format :
+'true' or 'false'
+Constraints :
+0 <= |S| <= 1000
+where |S| represents length of string S.
+Sample Input 1 :
+racecar
+Sample Output 1:
+true
+Sample Input 2 :
+ninja
+Sample Output 2:
+false
+ */
+
+import java.util.Scanner;
+
+public class AS14Q04_CP {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        System.out.println(check(s));
+    }
+    public static boolean check(String s){
+        if(s.length()<=1)return true;
+        if(s.charAt(0)==s.charAt(s.length()-1)){
+            return check(s.substring(1,s.length()-1));
+        }
+        else return false;
+    }
+}
